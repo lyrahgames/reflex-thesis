502499895ec981b0611e0952516b8526c5634926
lyrahgames <lyrahgames@mailbox.org>
2023-03-24 22:35:57 +0100

    Add some more information to polyhedral surfaces

--- sections/code/vertices-and-faces.hpp
+++ sections/code/vertices-and-faces.hpp
@@ -1,11 +1,24 @@
+// Vertices store their position and
+// a pseudo-normal to model additional attributes
+// and improve rendering quality.
+//
 struct vertex {
   vec3 position{};
   vec3 normal{};
 };
 
+// In OpenGL and other standard applications,
+// it is sufficient to use 32-bit unsigned integers
+// to reference vertices.
+//
 using vertex_id = uint32_t;
 
+// Each face is a contiguous array of three vertex indices.
+//
 struct face : array<vertex_id, 3> {};
 
+// The vertex and face list are contiguous arrays
+// which are dynamically allocated and managed on the heap.
+//
 vector<vertex> vertices{};
 vector<face> faces{};
