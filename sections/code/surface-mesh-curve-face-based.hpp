ff8b9122e5720a7651126b585528528c3c8e6ab4
lyrahgames <lyrahgames@mailbox.org>
2023-03-27 17:35:34 +0200

    Add more content to face-based curves

--- sections/code/surface-mesh-curve-face-based.hpp
+++ sections/code/surface-mesh-curve-face-based.hpp
@@ -1,10 +1,33 @@
-struct face_point {
+struct face_based_node {
   face_id fid;
+
+  // The location inside the adjacency of
+  // the face given by the reference 'fid'
+  // that points to the face adjacency
+  // representing the previous face in the strip.
+  //
   face_id loc : 16;
+
+  // The amount that needs to be added to 'loc'
+  // to get the location inside the adjacency of
+  // the face given by the reference 'fid'
+  // that points to the face adjacency
+  // representing the next face in the strip.
+  //
   face_id rot : 16;
 };
 
-struct surface_mesh_curve {
-  vector<face_point> face_strip{};
-  vector<float> edge_weights{};
-};
+struct face_based_surface_mesh_curve {
+  // The face strip is only a list face-based nodes
+  // and handled by contiguous array that is
+  // dynamically allocated and managed on the heap.
+  //
+  vector<face_based_node> face_strip{};
+
+  // The edge weights and mapping parameters
+  // need to be separated from the face strip
+  // and will be stored as separate arrays.
+  //
+  vector<float> w{};
+  vector<float> t{};
+} face_based_curve{};
