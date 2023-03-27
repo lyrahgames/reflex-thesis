db86b164ddf48cef6528b163b7a646dc5d1640f8
lyrahgames <lyrahgames@mailbox.org>
2023-03-27 16:04:09 +0200

    Add explanation for edge-based curve structures

--- sections/code/surface-mesh-curve-edge-based.hpp
+++ sections/code/surface-mesh-curve-edge-based.hpp
@@ -1,7 +1,22 @@
-struct control_point {
+struct edge_based_control_point {
+  // Directed edge pointing to the right side of the
+  // curve with references to topological vertices
+  //
   edge e{};
+
+  // Edge weight parameter specifying the exact
+  // location of the control point on the edge.
+  //
   float w{};
+
+  // Mapping parameter to be able to map values
+  // from the original curve to the current one.
+  //
   float t{};
 };
 
-vector<control_point> curve{};
+// The surface mesh curve is then simply a list of
+// control points represented by a contiguous array
+// dynamically allocated and managed on the heap.
+//
+vector<control_point> edge_based_curve{};
